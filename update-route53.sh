7f01c1db47cdfa492807425e6e39c24795e13347
young.un <duddns@gmail.com>
2017-09-06 16:23:35 +0900

    Update update-route53.sh

--- update-route53.sh
+++ update-route53.sh
@@ -15,7 +15,12 @@ HOSTED_ZONE_ID="{CHANGE_HOSTED_ZONE_ID}"
 
 IP=$( curl http://instance-data/latest/meta-data/public-ipv4 )
 
-INPUT_JSON=$( cat ./update-route53-A.json | sed "s/127\.0\.0\.1/$IP/" )
+JSON_DIR=$(dirname $(realpath $0))
+JSON_FILE=$JSON_DIR/update-route53-A.json
+echo "JSON_FILE: $JSON_FILE"
+
+INPUT_JSON=$( cat $JSON_FILE | sed "s/127\.0\.0\.1/$IP/" )
+echo "INPUT_JSON: $INPUT_JSON"
 
 # http://docs.aws.amazon.com/cli/latest/reference/route53/change-resource-record-sets.html
 # We want to use the string variable command so put the file contents (batch-changes file) in the following JSON
